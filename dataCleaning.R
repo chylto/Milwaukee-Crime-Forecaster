ee0729728e5931b05b20e50b09de5bff5f0a615b
KWeathington <katy.weathington@marquette.edu>
2020-02-15 12:58:03 -0600

    better cleaning

--- dataCleaning.R
+++ dataCleaning.R
@@ -1,14 +1,20 @@
-df <- na.omit(read.csv('mke_wibrs_db\\testdata.csv'))
+df <- na.omit(read.csv('testdata.csv'))
 head(df)
 tail(df)
 summary(df)
 
 
+df$year <- (substring(df$c_violation_date,first=1, last=4))
+df$month <- (substring(df$c_violation_date,first=6, last=7))
+df$day <-(substring(df$c_violation_date,first=9, last=10))
+df$date <- paste(df$day, df$month,df$year, sep="")
+df$date2 <- as.Date(df$date, "%d%m%Y")
+
 df$year <- as.numeric(substring(df$c_violation_date,first=1, last=4))
 df$month <- as.numeric(substring(df$c_violation_date,first=6, last=7))
 df$day <-as.numeric(substring(df$c_violation_date,first=9, last=10))
-df$date <- paste(df$day, df$month,df$year, sep="")
-df$date2 <- as.Date(df$date, "%d%m%Y")
+
+write.csv(df, file="cleanedData.csv")
 
 year <- c(2009:2014)
 months <-c(1:12)
