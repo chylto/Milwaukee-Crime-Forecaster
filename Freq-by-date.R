8db8ebc496b18efacc3ec003a4e1329cc6eabf54
KWeathington <katy.weathington@marquette.edu>
2020-02-20 13:04:24 -0600

    frequency dataset

--- Freq-by-date.R
+++ Freq-by-date.R
@@ -1,7 +1,13 @@
 library(lubridate)
 library(dplyr)
 
-pot2 <-read.csv("C:/Users/Caroline/Documents/GitHub/Milwaukee-Crime-Forecaster/cleanedData.csv")
+pot2 <-na.omit(read.csv("cleanedData.csv"))
 date=as.Date(pot2$date2)
 
-pot2 %>% group_by(date) %>% summarise(frequency = n())
+days <- pot2 %>% group_by(date) %>% summarise(frequency = n())
+d <- days$date
+for (i in 1:length(days$date)) {
+  if(length(days$date[i])<8){
+    days$date[i]=paste("0",days$date[i])
+  }
+}
--- dataCleaning.R
+++ dataCleaning.R
@@ -10,12 +10,22 @@ df$day <-(substring(df$c_violation_date,first=9, last=10))
 df$date <- paste(df$day, df$month,df$year, sep="")
 df$date2 <- as.Date(df$date, "%d%m%Y")
 
-df$year <- as.numeric(substring(df$c_violation_date,first=1, last=4))
-df$month <- as.numeric(substring(df$c_violation_date,first=6, last=7))
-df$day <-as.numeric(substring(df$c_violation_date,first=9, last=10))
+df$yearN <- as.numeric(substring(df$c_violation_date,first=1, last=4))
+df$monthN <- as.numeric(substring(df$c_violation_date,first=6, last=7))
+df$dayN <-as.numeric(substring(df$c_violation_date,first=9, last=10))
 
 write.csv(df, file="cleanedData.csv")
 
 year <- c(2009:2014)
 months <-c(1:12)
 days <- c(1:31)
+
+d <- df$date
+library(lubridate)
+
+dd<- df%>% group_by(date2) %>% summarise(frequency = n())
+dd$year <- year(dd$date2)
+dd$month <- month(dd$date2)
+dd$day <-day(dd$date2)
+
+write.csv(dd, file="frequencyData.csv")
