60de0b81215b11b89939d6125c9ade5afce4d01c
KWeathington <katy.weathington@marquette.edu>
2020-03-02 11:06:49 -0600

    barplots of crime

--- exploratoryAnalysis.R
+++ exploratoryAnalysis.R
@@ -4,3 +4,8 @@ model<-lm(df$frequency~df$y+df$m+df$d+df$dow)
 summary(model)
 #Turns out year and month have a significant relationship to number of events, and some days of the week have an influence but not all.
 #interestingly, the date in the month does not have a significant effect.
+
+barplot(table(df$y),main=("Number of Crime Events by Year"))
+barplot(table(df$m),main=("Number of Crime Events by Month"))
+barplot(table(df$d),main=("Number of Crime Events by Day of Month"))
+barplot(table(df$dow),main=("Number of Crime Events by Day of Week"))
